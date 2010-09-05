b922f208c76b32f5631e7fecf92608d21ca3216b
jspraul <none@none.com>
2010-09-05 02:52:30 +0000

    Cleaning up initial source control import from various source drops: after running Tree Trim (https://code.google.com/p/treetrim/) to remove temporary files, adding anything that was in one of the source code drops in an attempt to make sure there is a record of the file having existed at some point (not going to worry about which particular release referenced what, sorry)

---
+++ DBDiff.Testing/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DBDiff.Testing")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("EgSoft")> 
+<Assembly: AssemblyProduct("DBDiff.Testing")> 
+<Assembly: AssemblyCopyright("Copyright © EgSoft 2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bcbdb813-636a-48ac-b6a9-7063b9cd2dd5")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
