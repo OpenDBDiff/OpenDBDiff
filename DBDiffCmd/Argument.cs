bc948eceec79f5c94052930c924f46b6312b4f71
jspraul <none@none.com>
2010-09-05 01:32:40 +0000

    20080927 - OpenDBDiff 1.0 (Beta 5) http://opendbiff.codeplex.com/releases/view/17749#DownloadId=44665

---
+++ DBDiffCmd/Argument.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBDiff.OCDB
+{
+    public class Argument
+    {
+        private string connectionString1;
+        private string connectionString2;
+        private string outputFile;
+        
+        public Argument(string[] commandline)
+        {
+            for (int i = 0; i < commandline.Length; i++)
+            {
+                if (commandline[i].Length > 4)
+                {
+                    if (commandline[i].Substring(0, 4).Equals("CN1=", StringComparison.CurrentCultureIgnoreCase))
+                        connectionString1 = commandline[i].Substring(4, commandline[i].Length - 4).Trim();
+                    if (commandline[i].Substring(0, 4).Equals("CN2=", StringComparison.CurrentCultureIgnoreCase))
+                        connectionString2 = commandline[i].Substring(4, commandline[i].Length - 4).Trim();
+                    if (commandline[i].Substring(0, 2).Equals("F=", StringComparison.CurrentCultureIgnoreCase))
+                        outputFile = commandline[i].Substring(2, commandline[i].Length - 2).Trim();
+                }
+            }
+            if (String.IsNullOrEmpty(connectionString1) || String.IsNullOrEmpty(ConnectionString1) || String.IsNullOrEmpty(outputFile))
+            {
+                System.Console.WriteLine("Example of use:");
+                System.Console.WriteLine("OCDB CN1=\"Connection String 1\" CN2=\"Connection String 2\" F=FileDestination.sql");
+            }
+        }
+
+        public string OutputFile
+        {
+            get { return outputFile; }
+            set { outputFile = value; }
+        }
+
+        public string ConnectionString2
+        {
+            get { return connectionString2; }
+            set { connectionString2 = value; }
+        }
+
+        public string ConnectionString1
+        {
+            get { return connectionString1; }
+            set { connectionString1 = value; }
+        }
+
+        public bool Validate()
+        {
+            if (String.IsNullOrEmpty(connectionString1))
+                throw new Exception("The target connection string is missing");
+            if (String.IsNullOrEmpty(connectionString2))
+                throw new Exception("The destination connection string is missing");
+            if (String.IsNullOrEmpty(outputFile))
+                throw new Exception("The output destination is missing");
+            return true;
+        }
+    }
+}
---
+++ DBDiffCmd/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DBDiff.Schema.SQLServer.Options;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace DBDiff.OCDB
+{
+    public class Program
+    {
+        private static SqlOption SqlFilter = new SqlOption();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Argument arguments = new Argument(args);
+                if (arguments.Validate())
+                    Work(arguments);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+
+        static Boolean TestConnection(string connectionString1, string connectionString2)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = connectionString1;
+                connection.Open();
+                connection.Close();
+                connection.ConnectionString = connectionString2;
+                connection.Open();
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        static void Work(Argument arguments)
+        {
+            try
+            {
+                DBDiff.Schema.SQLServer.Model.Database origen;
+                DBDiff.Schema.SQLServer.Model.Database destino;
+                if (TestConnection(arguments.ConnectionString1, arguments.ConnectionString2))
+                {
+                    DBDiff.Schema.SQLServer.Generate sql = new DBDiff.Schema.SQLServer.Generate();
+                    sql.ConnectionString = arguments.ConnectionString1;
+                    System.Console.WriteLine("Reading first database...");                    
+                    origen = sql.Process(SqlFilter);                    
+                    sql.ConnectionString = arguments.ConnectionString2;
+                    System.Console.WriteLine("Reading second database...");
+                    destino = sql.Process(SqlFilter);
+                    System.Console.WriteLine("Comparing databases schemas...");
+                    origen = DBDiff.Schema.SQLServer.Generate.Compare(origen, destino);
+                    System.Console.WriteLine("Generating SQL file...");
+                    SaveFile(arguments.OutputFile, origen.ToSql());
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void SaveFile(string filenmame, string sql)
+        {
+            if (!String.IsNullOrEmpty(filenmame))
+            {
+                StreamWriter writer = new StreamWriter(filenmame, false);
+                writer.Write(sql);
+                writer.Close();
+            }
+        }
+    }
+}
