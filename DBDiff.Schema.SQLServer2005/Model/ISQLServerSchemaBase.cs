c76eb9c2f6e77f36faedeef070cb340b7038c532
jspraul <none@none.com>
2010-09-06 01:42:14 +0000

    Source control cleanup - remove unreferenced projects and files from source control.

--- DBDiff.Schema.SQLServer2005/Model/ISQLServerSchemaBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using DBDiff.Schema.Model;
-namespace DBDiff.Schema.SQLServer.Model
-{
-    public interface ISQLServerSchemaBase
-    {
-        SchemaList<ExtendedProperty, ISchemaBase> ExtendedProperties { get; }
-    }
-}
