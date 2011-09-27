849c091b7a5543a3afc5c18aeb51e5d62fcf3e21
jspraul <none@none.com>
2011-09-27 22:25:34 +0000

    Adds missing version info file to update to v8.9.2.

---
+++ DBDiff/Properties/AssemblyVersionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.8.9.2")]
+[assembly: AssemblyFileVersion("0.8.9.2")]
