1f1bdab367c488359258e14f7d6a03b39db5bc42
jspraul <none@none.com>
2012-03-15 12:08:25 +0000

    Builds as v8.9.5.

--- DBDiff/Properties/AssemblyVersionInfo.cs
+++ DBDiff/Properties/AssemblyVersionInfo.cs
@@ -7,5 +7,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.9.4")]
-[assembly: AssemblyFileVersion("0.8.9.4")]
+[assembly: AssemblyVersion("0.8.9.5")]
+[assembly: AssemblyFileVersion("0.8.9.5")]
