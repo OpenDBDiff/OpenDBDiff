423622579bba23ae5e0e1b3246999d9847eff35a
evangelalessandro <evangel.alessandro@gmail.com>
2017-06-05 15:28:44 +0200

    Revert "Formatted document"
    This reverts commit 96df27f9af44a3d16885f63278c8d0455cf93534.

--- DBDiff.Schema/Model/SearchSchemaBase.cs
+++ DBDiff.Schema/Model/SearchSchemaBase.cs
@@ -34,12 +34,11 @@ namespace DBDiff.Schema.Model
         }
 
 
-		public Nullable<Enums.ObjectType> GetType(string FullName)
-		{
-			if (objectTypes.ContainsKey(FullName.ToUpper()))
-				return objectTypes[FullName.ToUpper()];
-			return null;
-		}
+        public Nullable<Enums.ObjectType> GetType(string FullName) {
+            if(objectTypes.ContainsKey(FullName.ToUpper()))
+                return objectTypes[FullName.ToUpper()];
+            return null;
+        }
 
         public string GetParentName(string FullName)
         {
