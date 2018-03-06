ab0888faf4186ffb04d4aec7433b933932f309aa
Francois Botha <igitur@gmail.com>
2018-03-06 17:35:50 +0200

    Show and improve error form.

---
+++ OpenDBDiff/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenDBDiff.Front.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        // a.k.a., linked list style enumerator
+        public static IEnumerable<TSource> FromHierarchy<TSource>(
+            this TSource source,
+            Func<TSource, TSource> nextItem,
+            Func<TSource, bool> canContinue)
+        {
+            for (var current = source; canContinue(current); current = nextItem(current))
+            {
+                yield return current;
+            }
+        }
+
+        public static IEnumerable<TSource> FromHierarchy<TSource>(
+            this TSource source,
+            Func<TSource, TSource> nextItem)
+            where TSource : class
+        {
+            return FromHierarchy(source, nextItem, s => s != null);
+        }
+    }
+}
