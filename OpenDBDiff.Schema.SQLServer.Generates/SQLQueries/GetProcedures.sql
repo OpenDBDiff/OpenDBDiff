12f87ab473b661d14350371d7be2024ba8aec52b
Mark Gaze <mark.l.gaze@gmail.com>
2018-03-29 11:59:31 +0100

    Re-added SQL to retrieve the stored procs which was erroneously left out

--- OpenDBDiff.Schema.SQLServer.Generates/SQLQueries/GetProcedures.sql
+++ OpenDBDiff.Schema.SQLServer.Generates/SQLQueries/GetProcedures.sql
@@ -3,3 +3,8 @@ FROM sys.assembly_modules AM
 LEFT JOIN sys.objects O ON AM.object_id = O.object_id 
 INNER JOIN sys.schemas S ON S.schema_id = O.schema_id 
 LEFT JOIN sys.assemblies AF ON AF.assembly_id = AM.assembly_id AND AF.is_user_defined = 1
+UNION ALL
+SELECT 'CALLER' as ExecuteAs, P.type, NULL AS assembly_name, NULL, NULL, NULL, P.object_id, S.name as owner, P.name as name 
+FROM sys.procedures P 
+INNER JOIN sys.schemas S ON S.schema_id = P.schema_id 
+WHERE P.type = 'P'
