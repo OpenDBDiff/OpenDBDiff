a8468c5bdb486c45ae60dcaf7a40983b51a1e286
Mark Gaze <mark.l.gaze@gmail.com>
2018-03-30 11:50:24 +0100

    Corrected logic in the SchemaBase.Status (#74)
    * Corrected logic in the SchemaBase.Status
    
    * Added a unit test to prove the change fixes the issue
    
    * Revert 28c28c8
    
    * Updated Status to refactoring by @igitur
    
    * Fixed incorrect tabs to use spaces
    
    * Fixed more tabs
    
    * And the last tab fix

--- OpenDBDiff.Schema/Model/SchemaBase.cs
+++ OpenDBDiff.Schema/Model/SchemaBase.cs
@@ -225,18 +225,31 @@ namespace OpenDBDiff.Schema.Model
             get { return status; }
             set
             {
-                if ((status != ObjectStatus.Rebuild) && (status != ObjectStatus.RebuildDependencies))
+                if (status != ObjectStatus.Rebuild && status != ObjectStatus.RebuildDependencies)
                     status = value;
 
+                if (Parent == null) return;
+
                 // Si el estado de la tabla era el original, lo cambia, sino deja el actual estado.
-                if (Parent != null && Parent.Status == ObjectStatus.Original || value == ObjectStatus.Rebuild || value == ObjectStatus.RebuildDependencies)
+                // If the state of the table was the original, it changes it, but leaves the current state. (Google translated)
+                if (Parent.Status == ObjectStatus.Original
+                    || value == ObjectStatus.Rebuild
+                    || value == ObjectStatus.RebuildDependencies)
                 {
-                    if ((value != ObjectStatus.Original) && (value != ObjectStatus.Rebuild) && (value != ObjectStatus.RebuildDependencies))
-                        Parent.Status = ObjectStatus.Alter;
-                    if (value == ObjectStatus.RebuildDependencies)
-                        Parent.Status = ObjectStatus.RebuildDependencies;
-                    if (value == ObjectStatus.Rebuild)
-                        Parent.Status = ObjectStatus.Rebuild;
+                    switch (value)
+                    {
+                        case ObjectStatus.RebuildDependencies:
+                        case ObjectStatus.Rebuild:
+                            Parent.Status = value;
+                            break;
+
+                        case ObjectStatus.Original:
+                            break;
+
+                        default:
+                            Parent.Status = ObjectStatus.Alter;
+                            break;
+                    }
                 }
             }
         }
