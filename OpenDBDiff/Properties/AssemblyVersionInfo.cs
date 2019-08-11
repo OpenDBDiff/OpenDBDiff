6f540de48573c0f1af7384f86208f7905f53b29e
Francois Botha <igitur@gmail.com>
2019-08-11 17:08:41 +0200

    Bump version to 0.11

--- OpenDBDiff/Properties/AssemblyVersionInfo.cs
+++ OpenDBDiff/Properties/AssemblyVersionInfo.cs
@@ -7,5 +7,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("0.11.0.0")]
+[assembly: AssemblyFileVersion("0.11.0.0")]
