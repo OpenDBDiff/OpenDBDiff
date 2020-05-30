7afe459e0f9cb832521542bcca543b1fe191cf1e
Francois Botha <igitur@gmail.com>
2020-05-30 14:48:20 +0200

    Bump version to 0.20

--- OpenDBDiff/Properties/AssemblyVersionInfo.cs
+++ OpenDBDiff/Properties/AssemblyVersionInfo.cs
@@ -7,5 +7,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.11.0.0")]
-[assembly: AssemblyFileVersion("0.11.0.0")]
+[assembly: AssemblyVersion("0.20.0.0")]
+[assembly: AssemblyFileVersion("0.20.0.0")]
